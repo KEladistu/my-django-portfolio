fc5e08e35a906d6f9ef2c3a47818df0c4628ca8a
Kenneth Eladistu <kennetheladistu@gmail.com>
2025-02-08 15:57:55 +0800

    Added static images and resume PDFs

--- resume/views.py
+++ resume/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.staticfiles.storage import staticfiles_storage
 
-
 # Create your views here.
 def home(request):
     return render (request,"home.html")
@@ -10,47 +9,26 @@ def home(request):
 def about (request):
     return render (request,"about.html")
 
-
 def projects (request):
     projects_show=[
         {
-            'title': 'Rasoi Connect',
-            'path': 'images/rasoi_connect.PNG',
+            'title': 'Ice Cream Shop POS System',
+            'path': 'images/Ice Cream Shop POS.jpg',
         },
         {
-            'title': 'Ecommerce',
-            'path': 'images/ecommerce.PNG',
-        },
-
-        {
-            'title': 'Timetable Scheduler',
-            'path': 'images/timtable.PNG',
+            'title': 'Django Space Game',
+            'path': 'images/python space game.jpeg',
         },
         {
-            'title': 'CRUD',
-            'path': 'images/CRUD.PNG',
+            'title': 'Heartdroid',
+            'path': 'images/heartdroid.jpeg',
         },
-
-         {
-            'title': 'Photo Uploader',
-            'path': 'images/photo_uploader.PNG',
-        },
-          {
-            'title': 'To do list',
-            'path': 'images/todolist.PNG',
-        },
-         {
+        {
             'title': 'Portfolio',
-            'path': 'images/portfolio.PNG',
+            'path': 'images/Dashboard .png',
         },
-                  {
-            'title': 'Labour Hiring',
-            'path': 'images\labour_hiring.PNG',
-        },
-
     ]
     return render (request,"projects.html",{"projects_show": projects_show})
-
 
 def experience(request):
     experience=[
@@ -63,21 +41,19 @@ def experience(request):
     ]
     return render (request,"experience.html",{"experience":experience})
 
-
 def certificate(request):
     return render (request, "certificate.html")
-
 
 def contact(request):
     return render (request,"contact.html")
 
 def resume(request):
-    resume_path="myapp/resume.pdf"
+    resume_path="myapp/resumekennetheladistu.pdf"
     resume_path=staticfiles_storage.path(resume_path)
     if staticfiles_storage.exists(resume_path):
         with open(resume_path,"rb") as resume_file:
             response=HttpResponse(resume_file.read(),content_type="application/pdf")
-            response['Content-Disposition']='attachment';filename="resume.pdf"
+            response['Content-Disposition']='attachment';filename="resumekennetheladistu.pdf"
             return response
     else:
         return HttpResponse("resume not found", status=404)
